3afd7b7c86598d4dbf19cb95372b024bfc516d3b
AlexandrIvanov <alexandr.1996@list.ru>
2023-06-02 17:25:44 +0400

    Add Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import './scss/app.scss';
-import {Header} from './components/Header/Header';
 import {Route, Routes} from 'react-router-dom';
 import {Home} from './pages/Home/Home';
 import {Cart} from './pages/Cart/Cart';
 import {NotFoundPage} from './pages/NotFoundPage';
 import {PizzaItem} from './pages/PizzaItem/PizzaItem';
+import {Layout} from './pages/Layout';
 
 function App() {
     return (
-        <div className="wrapper">
-                <Header/>
-                <div className="content">
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="/cart" element={<Cart/>}/>
-                        <Route path="/pizza/:id" element={<PizzaItem/>}/>
-                        <Route path="/*" element={<NotFoundPage/>}/>
-                    </Routes>
-                </div>
-        </div>
+        <Routes>
+            <Route path="/" element={<Layout/>}>
+                <Route path="" element={<Home/>}/>
+                <Route path="cart" element={<Cart/>}/>
+                <Route path="pizza/:id" element={<PizzaItem/>}/>
+                <Route path="*" element={<NotFoundPage/>}/>
+            </Route>
+        </Routes>
     );
 }
 
